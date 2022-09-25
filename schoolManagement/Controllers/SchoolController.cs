58c1d52db9660fec087a9bfca5352e91b9664e10
Zblackeur <cedriczambo2412@gmail.com>
2022-09-25 22:45:05 +0100

    fin TP4

---
+++ schoolManagement/Controllers/SchoolController.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using schoolManagement.Models;
+using schoolManagement.Repositories;
+
+namespace schoolManagement.Controllers
+{
+    [Authorize(Roles = "Manager,Admin")]
+    public class SchoolController : Controller
+    {
+        readonly ISchoolRepository schoolRepository;
+        public SchoolController(ISchoolRepository schoolRepository)
+        {
+            this.schoolRepository = schoolRepository;
+        }
+
+        // GET: SchoolController
+        [AllowAnonymous]
+        public ActionResult Index()
+        {
+            return View(schoolRepository.GetAll());
+        }
+
+        // GET: SchoolController/Details/5
+        public ActionResult Details(int id)
+        {
+            var school = schoolRepository.GetById(id);
+            return View(school);
+        }
+
+        // GET: SchoolController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: SchoolController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(School s)
+        {
+            try
+            {
+                schoolRepository.Add(s);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: SchoolController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var school = schoolRepository.GetById(id);
+            return View(school);
+        }
+
+        // POST: SchoolController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(School s)
+        {
+            try
+            {
+                schoolRepository.Edit(s);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: SchoolController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var school = schoolRepository.GetById(id);
+            return View(school);
+        }
+
+        // POST: SchoolController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(School s)
+        {
+            try
+            {
+
+                schoolRepository.Delete(s);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ schoolManagement/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace schoolManagement.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
